b309370e3554297e3c37950dfae4ed8993960691
Codreanu Cezar <cezar.codreanu06@gmail.com>
2026-03-07 21:38:27 +0200

    De rezolvat bug la player movement sincronizat

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Netcode; 
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class MeniuManager : MonoBehaviour
+{
+    public Button butonHost;
+    public Button butonClient;
+
+    private void Start()
+    {
+        butonHost.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+            AscundeMeniul();
+        });
+
+        butonClient.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+            AscundeMeniul();
+        });
+    }
+
+    void AscundeMeniul()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerControl : NetworkBehaviour
+{
+    public float viteza = 5f;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (!IsOwner) return;
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 miscare = new Vector2(moveX, moveY).normalized;
+
+        rb.linearVelocity = miscare * viteza;
+
+        if (anim != null)
+        {
+            anim.SetFloat("Speed", miscare.magnitude);
+        }
+
+        if (moveX != 0)
+        {
+            spriteRenderer.flipX = moveX < 0;
+        }
+    }
+}
