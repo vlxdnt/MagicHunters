81d93373bed2155522549f9ec494ca17a207495f
Codreanu Cezar <cezar.codreanu06@gmail.com>
2026-03-13 08:58:11 +0200

    Teste de server

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -1,31 +1,59 @@
 ﻿using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class LobbySelection : NetworkBehaviour
 {
     // 0 = Nimic, 1 = Witch, 2 = Cat
     public NetworkVariable<int> hostSelection = new NetworkVariable<int>(0);
     public NetworkVariable<int> clientSelection = new NetworkVariable<int>(0);
+    public NetworkVariable<int> nrJucatori = new NetworkVariable<int>(0);
 
     public Button butonWitch;
     public Button butonCat;
+    public TextMeshProUGUI textJucatori;
 
-    public void AlegeWitch() { TrimiteAlegereaServerRpc(1); }
-    public void AlegeCat() { TrimiteAlegereaServerRpc(2); }
-
-    [ServerRpc(RequireOwnership = false)]
-    void TrimiteAlegereaServerRpc(int choice, ServerRpcParams rpcParams = default)
+    public override void OnNetworkSpawn()
     {
-        if (rpcParams.Receive.SenderClientId == 0) // E Hostul
+        if (IsServer)
+        {
+            nrJucatori.Value = NetworkManager.Singleton.ConnectedClients.Count;
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        }
+    }
+
+    void OnClientConnected(ulong clientId)
+    {
+        nrJucatori.Value = NetworkManager.Singleton.ConnectedClients.Count;
+    }
+
+    public void AlegeWitch() { TrimiteAlegereaRpc(1, NetworkManager.Singleton.LocalClientId); }
+    public void AlegeCat() { TrimiteAlegereaRpc(2, NetworkManager.Singleton.LocalClientId); }
+
+    [Rpc(SendTo.Server, InvokePermission = RpcInvokePermission.Everyone)]
+    void TrimiteAlegereaRpc(int choice, ulong clientId)
+    {
+        if (clientId == 0)
             hostSelection.Value = choice;
-        else // E Clientul
+        else
             clientSelection.Value = choice;
     }
 
     void Update()
     {
-        // Dezactivăm butoanele dacă celălalt a ales deja
+        if (textJucatori != null)
+            textJucatori.text = "Jucatori conectati: " + nrJucatori.Value + "/2";
+
+        if (NetworkManager.Singleton.IsHost)
+        {
+            MeniuManager meniu = FindFirstObjectByType<MeniuManager>();
+            if (meniu != null && meniu.butonStart != null)
+                meniu.butonStart.interactable = (nrJucatori.Value == 2);
+        }
+
+        if (butonWitch == null || butonCat == null) return;
+
         if (IsHost)
         {
             butonWitch.interactable = (clientSelection.Value != 1);
@@ -36,6 +64,5 @@ public class LobbySelection : NetworkBehaviour
             butonWitch.interactable = (hostSelection.Value != 1);
             butonCat.interactable = (hostSelection.Value != 2);
         }
-        //test
     }
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,27 +1,92 @@
-﻿using Unity.Netcode; 
+﻿using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
+using TMPro;
+using System.Net;
+using System.Net.Sockets;
 
 public class MeniuManager : MonoBehaviour
 {
-    public Button butonHost;
-    public Button butonClient;
+    [Header("Panouri")]
+    public GameObject panelMeniu;
+    public GameObject panelLobby;
+
+    [Header("Input & Info")]
+    public TMP_InputField inputFieldIP;   // InputIP
+    public Button inputButton;             // InputButton
+    public TextMeshProUGUI textIPLocal;
+
+    [Header("Butoane Lobby")]
+    public Button butonStart; // Doar host il vede
 
     private void Start()
     {
-        butonHost.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartHost();
-            AscundeMeniul();
-        });
-
-        butonClient.onClick.AddListener(() => {
-            NetworkManager.Singleton.StartClient();
-            AscundeMeniul();
-        });
+        panelMeniu.SetActive(true);
+        panelLobby.SetActive(false);
+        inputFieldIP.gameObject.SetActive(false);
+        inputButton.gameObject.SetActive(false);
+        DontDestroyOnLoad(gameObject);
     }
 
-    void AscundeMeniul()
+    // Butonul HOST
+    public void ButonHost_Apasat()
     {
-        gameObject.SetActive(false);
+        NetworkManager.Singleton.StartHost();
+        textIPLocal.text = "IP-ul tau: " + GetLocalIPAddress();
+        AfiseazaLobby();
+    }
+
+    // Butonul JOIN - deschide panoul cu input field pentru IP
+    public void ButonDeschideIntroducereIP()
+    {
+        inputFieldIP.gameObject.SetActive(true);
+        inputButton.gameObject.SetActive(true);
+    }
+
+    // Butonul OK din panoul de IP
+    public void ButonFinalizeazaConexiuneClient()
+    {
+        var transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        if (transport != null && !string.IsNullOrEmpty(inputFieldIP.text))
+        {
+            transport.ConnectionData.Address = inputFieldIP.text;
+        }
+        NetworkManager.Singleton.StartClient();
+        AfiseazaLobby();
+    }
+
+    // Butonul START (doar host)
+    public void ButonStartJoc()
+    {
+        if (NetworkManager.Singleton.IsServer)
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", UnityEngine.SceneManagement.LoadSceneMode.Single);
+        }
+    }
+
+    void AfiseazaLobby()
+    {
+        panelMeniu.SetActive(false);
+        panelLobby.SetActive(true);
+        inputFieldIP.gameObject.SetActive(false);
+        inputButton.gameObject.SetActive(false);
+
+        if (butonStart != null)
+        {
+            butonStart.gameObject.SetActive(NetworkManager.Singleton.IsHost);
+            butonStart.interactable = false; // blocat pana sunt 2 jucatori
+        }
+    }
+
+    public string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+                return ip.ToString();
+        }
+        return "127.0.0.1";
     }
 }
